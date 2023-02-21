2c165c526b5caf01a49d3ada4fd95d0f6de12466
NicoGit333 <nicolas4.1999@hotmail.com>
2023-02-21 20:08:16 +0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# PI01_DATA07
- 
+ # PI01_DATA07 
+ ## Data Engeneering y Data Science
+
+ Para este primer proyecto de Ingenieria de Datos y Ciencia de Datos, voy a obtener 4 datasets de peliculas y series los cuales son: Netflix, Amazon, Hulu y Disney. Los cuales tienen informacion como Fecha de Estreno, Descripcion, Rating, Audiencia, ect. 
+
+ Con esta informacion la idea es hacer un deployment con FastAPI para que otras personas del equipo puedan consultar informacion, el tipo de informacion que se va a poder consultar es la siguiente: 
+
+ - Pelicula con mayor duracion (con determinados filtros)
+ - Cantidad de peliculas por plataforma con un puntaje mayor a XX
+ - Cantidad de peliculas por plataforma 
+ - Actor mas repetido por plataforma y año
+
+ Tambien la idea es hacer un analisis de esta informacion, para despues crear un sistema de recomendacion, en el que dependiendo de un ID de un usuario y una pelicula dados, nos diga si se la recomendamos a este usuario o no.
---
+++ app/main.py
@@ -0,0 +1,66 @@
+import pandas as pd 
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def listaPalabrasDicFrec(listaPalabras):
+    frecuenciaPalab = [listaPalabras.count(p) for p in listaPalabras]
+    return dict(list(zip(listaPalabras, frecuenciaPalab)))
+
+
+def ordenaDicFrec(dicFrec):
+    aux = [(dicFrec[key], key) for key in dicFrec]
+    aux.sort()
+    aux.reverse()
+    return aux
+
+
+
+@app.get("/")
+def root():
+    return {'Hello': 'World'}
+
+
+@app.get('/get_max_duration')
+def get_max_duration(anio:int, platform:str, type:str): 
+    data = pd.read_csv('https://raw.githubusercontent.com/NicoGit333/Prueba_PI01/main/df_data.csv')
+    data.duration_int = pd.to_numeric(data.duration_int, errors = 'coerce')
+    data.release_year = pd.to_numeric(data.release_year, errors = 'coerce')
+    query1 = data[(data['release_year'] == anio) & (data['plataforma'] == platform) & (data['duration_type'] == type)]
+    query1 = query1.sort_values('duration_int', ascending=False)
+    res = query1.head(1)
+    res = res.title.to_list()
+
+    return 'La pelicula que mas duro fue: ', str(''.join(res))
+
+
+@app.get('/get_count_platform')
+def get_count_platform(platform:str):
+    data = pd.read_csv('https://raw.githubusercontent.com/NicoGit333/Prueba_PI01/main/df_data.csv')
+    query2 = data['plataforma'] == platform
+    count_query2 = data[query2]['type'].value_counts()
+
+    return {'Plataforma':platform,
+            'Movies':str(count_query2[0]),
+            'Shows':str(count_query2[1])
+            }
+
+@app.get('/get_actor')
+def get_actor(platform:str, anio:int):
+    data = data = pd.read_csv('https://raw.githubusercontent.com/NicoGit333/Prueba_PI01/main/df_data.csv')
+
+    act = data[(data['plataforma'] == platform) & (data['release_year'] == anio)].cast.str.split(',')
+    act = act.dropna()
+
+    actores_año = []
+    for actores in act: 
+        for actor in actores:
+            actor = actor.rstrip()
+            actor = actor.lstrip()
+            actores_año.append(actor)
+
+    actor = listaPalabrasDicFrec(actores_año)
+    actor = ordenaDicFrec(actor)
+
+    return f'El actor que mas se repite en: {platform} en el año: {anio} es: {actor[0][1]} con un total de: {actor[0][0]} apariciones.'
